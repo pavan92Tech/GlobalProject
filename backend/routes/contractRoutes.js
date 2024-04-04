cf99e30d2622af03bffe7b8b21dda10de65b0ac0
rvkn-patil <ravikiran.uvce2@gmail.com>
2024-04-04 14:46:24 +0530

    authentication added for all routes

--- backend/routes/contractRoutes.js
+++ backend/routes/contractRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const {
   registerContract, getAllContracts, getContract, updateContract, deleteContract
 } = require("../controllers/contractController");
+const validateToken = require("../middleware/validateTokenHandler");
 
 const router = express.Router();
 
+router.use(validateToken);
 router.post("/", registerContract);
 router.get("/", getAllContracts);
 router.get("/:id", getContract).put("/:id", updateContract).delete("/:id", deleteContract);
